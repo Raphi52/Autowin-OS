9f329d5358607f2e7acc0fd0f6a145e9fcd42a3d
Raphaël VILAIN <raphael.vilain@amitel365.onmicrosoft.com>
2026-09-04 16:44:44 +0200

    agent command-edit-conv-257-chatcomposer-tsx-19r3u7b

--- src/renderer/src/components/ChatComposer.tsx
+++ src/renderer/src/components/ChatComposer.tsx
@@ -114,8 +114,8 @@ function IconeEnvoyer(): React.JSX.Element {
   return (
     <svg
       viewBox="0 0 24 24"
-      width="16"
-      height="16"
+      width="19"
+      height="19"
       fill="none"
       stroke="currentColor"
       strokeWidth="2"
